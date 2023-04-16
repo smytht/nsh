7e7a1cb0155b9bb5be7e1a3b935267acabe537dd
smytht <smytht@users.noreply.github.com>
2023-04-16 12:27:28 +0100

    Codechecker  argument vs print format
    [MEDIUM] /home/tom/1.1.43/nsh/stats.c:851:2: %u in format string (no. 1) requires 'unsigned int' but the argument type is 'signed int'. [cppcheck-invalidPrintfArgType_uint]
      printf("\t%u mbuf%s in use:\n", totmbufs, plural(totmbufs));
      ^
    
    [MEDIUM] /home/tom/1.1.43/nsh/stats.c:876:2: %u in format string (no. 1) requires 'unsigned int' but the argument type is 'signed int'. [cppcheck-invalidPrintfArgType_uint]
      printf("\t%u Kbytes allocated to network (%d%% in use)\n",
      ^
    
    Found 2 defect(s) in stats.c

--- stats.c
+++ stats.c
@@ -860,7 +860,7 @@ mbpr(void)
 	seen[MT_FREE] = YES;
 	for (i = 0; i < nmbtypes; i++)
 		if (!seen[i] && mbstat.m_mtypes[i]) {
-			printf("\t\t%u mbuf%s allocated to <mbuf type %d>\n",
+			printf("\t\t%d mbuf%s allocated to <mbuf type %d>\n",
 			    mbstat.m_mtypes[i],
 			    plural((int)mbstat.m_mtypes[i]), i);
 		}
@@ -873,7 +873,7 @@ mbpr(void)
 	totused = mbpool.pr_nout * mbpool.pr_size +
 	    mclpool.pr_nout * mclpool.pr_size;
 	totpct = (totmem == 0)? 0 : ((totused * 100)/totmem);
-	printf("\t%u Kbytes allocated to network (%d%% in use)\n",
+	printf("\t%d Kbytes allocated to network (%d%% in use)\n",
 	    totmem / 1024, totpct);
 	printf("\t%lu requests for memory denied\n", mbstat.m_drops);
 	printf("\t%lu requests for memory delayed\n", mbstat.m_wait);
